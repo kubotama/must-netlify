e50d09630a17b83c3c3ffec1ff8fbd991481a24f
kubotama <16697573+kubotama@users.noreply.github.com>
2020-03-14 22:59:43 +0900

    netlifyのプレビュー環境のテストを追加

--- tests/unit/title.spec.js
+++ tests/unit/title.spec.js
@@ -12,3 +12,14 @@ describe('ローカル環境のindex.html', () => {
     })
   })
 })
+
+describe('プレビュー環境のindex.html', () => {
+  it('タイトル', done => {
+    const title ='MarkUp Support Tool by netlify'
+    const netlifyUrl = 'https://deploy-preview-5--must-kubotama.netlify.com/'
+    request(netlifyUrl, (error, response, body) => {
+      expect(new JSDOM(body).window.document.getElementsByTagName('title')[0].textContent).toBe(title)
+      done()
+    })
+  })
+})
