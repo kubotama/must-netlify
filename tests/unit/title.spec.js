846be16daba8ebf969e850baf394def42b9e2aa7
kubotama <16697573+kubotama@users.noreply.github.com>
2020-03-14 22:47:14 +0900

    ローカル環境でindex.htmlのタイトルが正しく設定されていることを確認

---
+++ tests/unit/title.spec.js
@@ -0,0 +1,14 @@
+import { JSDOM } from 'jsdom'
+// import axios from 'axios'
+import request from 'request'
+
+describe('ローカル環境のindex.html', () => {
+  it('タイトル',  done => {
+    const title ='MarkUp Support Tool by netlify'
+    const localUrl = 'http://localhost:8080/'
+    request(localUrl, (error, response, body) => {
+      expect(new JSDOM(body).window.document.getElementsByTagName('title')[0].textContent).toBe(title)
+      done()
+    })
+  })
+})
