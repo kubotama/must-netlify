cc43843e9a0056500094ca54f962e4e1b79c7263
kubotama <16697573+kubotama@users.noreply.github.com>
2020-03-15 16:52:50 +0900

    テストスクリプトの枠組み

---
+++ tests/unit/md.escape.spec.js
@@ -0,0 +1,8 @@
+import { JSDOM } from 'jsdom'
+import request from 'request'
+
+describe('Markdownの特殊文字をエスケープする。', () => {
+  it('MustUiコンポーネントが存在する。', () => {
+    expect(true).toBe(true)
+  })
+})
