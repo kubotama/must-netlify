11be14dff228d3ad6f76f7edbb265c82e03c88bc
kubotama <16697573+kubotama@users.noreply.github.com>
2020-03-21 23:21:20 +0900

    ボタンのID定義を一つにまとめる。

--- tests/unit/md.link.spec.js
+++ tests/unit/md.link.spec.js
@@ -2,18 +2,19 @@ import { shallowMount } from '@vue/test-utils'
 import MustUi from '@/components/MustUi.vue'
 
 describe('mdLinkButtonボタン', () => {
+  const idMdLinkButton = '#mdLinkButton'
   let wrapper
   beforeEach(() => {
     wrapper = shallowMount(MustUi)
   })
   it('存在する。', () => {
-    expect(wrapper.find('#mdLinkButton').exists()).toBeTruthy()
+    expect(wrapper.find(idMdLinkButton).exists()).toBeTruthy()
   })
 
   it('クリックするとonMdLinkが呼び出される。', () => {
     const onMdLink = jest.fn()
     wrapper.setMethods({ onMdLink })
-    wrapper.find('#mdLinkButton').trigger('click')
+    wrapper.find(idMdLinkButton).trigger('click')
     expect(onMdLink).toHaveBeenCalledTimes(1)
   })
 })
