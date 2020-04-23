c410e24416b1861cadfffe7848a151975036a57b
kubotama <16697573+kubotama@users.noreply.github.com>
2020-04-23 23:28:19 +0900

    GREEN: ヘッダにCORSを追加する条件を追加することでテストを成功した。

--- src/lambda/title.js
+++ src/lambda/title.js
@@ -3,32 +3,29 @@ import cheerio from "cheerio"
 import axios from "axios"
 
 export async function handler(event) {
+  const returnData = { headers: [] };
   const url = event.queryStringParameters.url;
   console.log("url: " + url)
   try {
     if (url.length == 0) {
-      return {
-        statusCode: 204,
-        body: ""
-      }
+      returnData.statusCode = 204
     }
-    const response = await axios.get(url)
-    const body = response.data
-    const $ = cheerio.load(body)
-    const title = $('title').text()
-    console.log("title: " + title)
-    return {
-      "headers": {
-        "Access-Control-Allow-Origin": event.headers.origin
-      },
-      "statusCode": 200,
-      body: title
+    else {
+      const response = await axios.get(url)
+      const body = response.data
+      const $ = cheerio.load(body)
+      const title = $('title').text()
+      console.log("title: " + title)
+      returnData.statusCode = 200
+      returnData.body = title
+      console.log(event.headers)
+      if (event.headers.host === "localhost:9000" && event.headers["user-agent"] != "axios/0.19.2") {
+        returnData.headers["Access-Control-Allow-Origin"] = event.headers.origin
+      }
     }
   } catch (error) {
     console.log(error)
-    return {
-      statusCode: 204,
-      body: ""
-    }
+    returnData.statusCode = 204;
   }
+  return returnData;
 }
